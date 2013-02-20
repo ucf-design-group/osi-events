9c1d19043671aa40e60393d0ef21d75ebe8ab200
AJ Foster <web@aj-foster.com>
2013-02-20 11:54:26 -0500

    Update the test pages to add events into their proper positions on the page

--- ajaxtest.js
+++ ajaxtest.js
@@ -1,4 +1,4 @@
-function retrieveEvents() {
+function retrieveEvents(key, start, end) {
 
 	jQuery.ajax({
 		type: "GET",
@@ -17,10 +17,31 @@ function retrieveEvents() {
 }
 
 function parseXML(xml) {
+
+	var events = new Array();
 	
 	jQuery(xml).find("event").each(function() {
 
-		var string = "Title: " + jQuery(this).find("title").text() + "<br />"
+		var singleEvent = new Array();
+
+		singleEvent['title'] = jQuery(this).find("title").text();
+		singleEvent['description'] = jQuery(this).find("description").text();
+		singleEvent['start'] = jQuery(this).find("start").text();
+		singleEvent['end'] = jQuery(this).find("end").text();
+		singleEvent['contact'] = jQuery(this).find("contact").text();
+		singleEvent['url'] = jQuery(this).find("url").text();
+		singleEvent['notes'] = jQuery(this).find("notes").text();
+
+		singleEvent['start'] = parseInt(singleEvent['start']);
+
+		if (singleEvent['end'] == "none")
+			singleEvent['end'] = -1;
+		else
+			singleEvent['end'] = parseInt(singleEvent['end']);
+
+		events.push(singleEvent);
+
+		/*var string = "Title: " + jQuery(this).find("title").text() + "<br />"
 			+ "Description: " + jQuery(this).find("description").text() + "<br />"
 			+ "Start UTC: " + jQuery(this).find("start").text() + "<br />"
 			+ "End UTC: " + jQuery(this).find("end").text() + "<br />"
@@ -28,7 +49,49 @@ function parseXML(xml) {
 			+ "URL: " + jQuery(this).find("url").text() + "<br />"
 			+ "Notes: " + jQuery(this).find("notes").text() + "<br /><br />";
 
-		jQuery("#here").append(string);
+		jQuery("#here").append(string);*/
 	});
 
+	/*for (var i = 0; i < events.length; i++) {
+		for (value in events[i]) {
+			jQuery("#here").append(value + " => " + events[i][value] + "<br>");
+		}
+		jQuery("#here").append("<br>");
+	}*/
+
+	addEvents(events);
+}
+
+function addEvents(events) {
+
+	var here = jQuery("#here");
+	var onPage = new Array();
+
+	here.find(".event").each(function() {
+		var eventInfo = new Array();
+		eventInfo['start'] = parseInt(jQuery(this).attr("id"));
+		eventInfo['title'] = jQuery(this).find(".title").text();
+		onPage.push(eventInfo);
+	});
+
+	var eventsOnPageRemaining = onPage.length;
+	for (var i = 0; i < eventsOnPageRemaining; i++) {
+		var evt = onPage[i];
+		for (var j = 0; j < events.length; j++) {
+			var newEvt = events[0];
+			//alert(newEvt['start'] + " " + newEvt['title'] + " " + evt['start'] + " " + evt['title']);
+			if (newEvt['start'] < evt['start'] && newEvt['title'] != evt['title']) {
+				jQuery("#" + evt['start']).before('<div class="event" id="' + newEvt['start'] + '"><div class="title">' + newEvt['title'] + '</div></div>');
+				events.splice(0, 1);
+			}
+		}
+	}
+
+	var eventsRemaining = events.length;
+	for (var i = 0; i < eventsRemaining; i++) {
+		var newEvt = events[0];
+		//alert(events[0]['start']);
+		jQuery("#here").append('<div class="event" id="' + newEvt['start'] + '"><div class="title">' + newEvt['title'] + '</div></div>');
+		events.splice(0, 1);
+	}
 }
